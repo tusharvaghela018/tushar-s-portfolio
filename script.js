2d79a6e6e00cd54654bfa29df236b84752915ff7
tusharvaghela018 <141395036+tusharvaghela018@users.noreply.github.com>
2024-02-14 16:52:44 +0530

    tushar's portfolio

---
+++ script.js
@@ -0,0 +1,66 @@
+//--------------------toggle icon navbar----------------------------------
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('Active');
+};
+
+//--------------------scroll section link active link----------------------------------
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('Active');
+                document.querySelector('header nav a[href*= ' + id +']').classList.add('Active');
+            });
+        }
+    });
+
+    //--------------------Sticky navbar----------------------------------
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    //--------------------remove toggle icon and navbar when click on links navbar----------------------------------
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('Active');
+
+};
+
+// scroll reveal
+
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200 
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+
+    ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'bottom'});
+
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+    var typed = new Typed(".multiple-text", {
+        strings: ["Frontend Developer...", "UI/UX Designer...", "Frontend Developer..."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+    
